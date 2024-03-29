f9cac0accb9cc1976cf453049b5261c2aaa58512
Jay-Sin <jay@DESKTOP-0Q9HLUV>
2024-03-28 20:41:29 -0400

    attempt to fix prod environment login bug. add getURL function to attempt to get vercel url

--- components/auth/sign-in-button.tsx
+++ components/auth/sign-in-button.tsx
@@ -3,6 +3,18 @@
 import { createBrowserClient } from "@/lib/supabase/clients/browser-client";
 import { Button } from "@/components/ui/button";
 
+const getURL = () => {
+    let url =
+        process?.env?.NEXT_PUBLIC_SITE_URL ?? // Set this to your site URL in production env.
+        process?.env?.NEXT_PUBLIC_VERCEL_URL ?? // Automatically set by Vercel.
+        "http://localhost:3000/";
+    // Make sure to include `https://` when not localhost.
+    url = url.includes("http") ? url : `https://${url}`;
+    // Make sure to include a trailing `/`.
+    url = url.charAt(url.length - 1) === "/" ? url : `${url}/`;
+    return url;
+};
+
 export const SignInButton = () => {
     const supabase = createBrowserClient();
 
@@ -10,7 +22,7 @@ export const SignInButton = () => {
         await supabase.auth.signInWithOAuth({
             provider: "discord",
             options: {
-                redirectTo: `${window.location.origin}/api/auth/callback`,
+                redirectTo: `${getURL()}/api/auth/callback`,
                 scopes: "identify email guilds guilds.members.read",
             },
         });
