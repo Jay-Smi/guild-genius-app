6703e8ce64311193bc8318f491777d690b43d6f9
Jay-Sin <jay@DESKTOP-0Q9HLUV>
2024-03-28 21:38:24 -0400

    another attempt to fix login prod bug

--- app/api/auth/callback/route.ts
+++ app/api/auth/callback/route.ts
@@ -14,53 +14,51 @@ export async function GET(request: NextRequest) {
     if (error)
         return NextResponse.redirect(`${requestUrl.origin}?error=${error}`);
 
-    if (code) {
-        const supabase = createServerClient<Database>(
-            process.env.NEXT_PUBLIC_SUPABASE_URL!,
-            process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-            {
-                cookies: {
-                    get(name: string) {
-                        return cookieStore.get(name)?.value;
-                    },
-                    set(name: string, value: string, options: CookieOptions) {
-                        cookieStore.set({ name, value, ...options });
-                    },
-                    remove(name: string, options: CookieOptions) {
-                        cookieStore.set({ name, value: "", ...options });
-                    },
-                },
-            }
-        );
-
-        const tokenResponse = await supabase.auth.exchangeCodeForSession(code);
-
-        console.log("api/auth/callback");
-
-        if (
-            tokenResponse &&
-            tokenResponse.data &&
-            tokenResponse.data.session &&
-            tokenResponse.data.session.provider_token
-        ) {
-            const providerToken = tokenResponse.data?.session?.provider_token;
-            console.log({ providerToken });
-            cookieStore.set("oauth_provider_token", providerToken, {
-                maxAge: 1000 * 60 * 60 * 24 * 30,
-                path: "/",
-                sameSite: "none",
-                secure: true,
-                httpOnly: true,
-            });
-        }
+    if (!code) {
+        return NextResponse.redirect(`${requestUrl.origin}/`, {
+            status: 302,
+        });
     }
+    const supabase = createServerClient<Database>(
+        process.env.NEXT_PUBLIC_SUPABASE_URL!,
+        process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+        {
+            cookies: {
+                get(name: string) {
+                    return cookieStore.get(name)?.value;
+                },
+                set(name: string, value: string, options: CookieOptions) {
+                    cookieStore.set({ name, value, ...options });
+                },
+                remove(name: string, options: CookieOptions) {
+                    cookieStore.set({ name, value: "", ...options });
+                },
+            },
+        }
+    );
 
+    const tokenResponse = await supabase.auth.exchangeCodeForSession(code);
+
+    const providerToken = tokenResponse.data?.session?.provider_token;
+
+    if (
+        tokenResponse &&
+        tokenResponse.data &&
+        tokenResponse.data.session &&
+        tokenResponse.data.session.provider_token
+    ) {
+        cookieStore.set("oauth_provider_token", providerToken || "", {
+            maxAge: 1000 * 60 * 60 * 24 * 30,
+            path: "/",
+            sameSite: "none",
+            secure: true,
+            httpOnly: true,
+        });
+    }
     return NextResponse.redirect(`${requestUrl.origin}/manage/my-guilds`, {
         status: 302,
         headers: {
-            "Set-Cookie": `oauth_provider_token=${cookieStore.get(
-                "oauth_provider_token"
-            )}; Path=/; Max-Age=2592000; SameSite=None; Secure; HttpOnly`,
+            "Set-Cookie": `oauth_provider_token=${providerToken}; Path=/; Max-Age=2592000; SameSite=None; Secure; HttpOnly`,
         },
     });
 }
